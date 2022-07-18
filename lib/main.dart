cfed83bb0b5199618ec683a1788b367fc1b23308
nabin sah <nabinsah703@gmail.com>
2022-07-18 10:21:01 +0545

    seperate personal form and create a dropdown list and add button form in this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sapati/transaction_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,22 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
     );
   }
 }
